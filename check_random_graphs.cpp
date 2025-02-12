c50308d56e04d1411f21a70af0c8553c2be673fc
Dmitry Koptev <dpkoptev@edu.hse.ru>
2025-02-12 19:10:05 +0300

    check_random_graphs.cpp: calculating in multiple threads, taking range of n values

--- check_random_graphs.cpp
+++ check_random_graphs.cpp
@@ -72,27 +72,68 @@ int main(int argc, char *argv[]) {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
-    if (argc != 3) {
-        cerr << "Usage: `{program_name} n count`\n";
+    if (argc < 4 || argc > 5) {
+        cerr << "Usage: `{program_name} n_l n_r graph_count [thread_count]`. \n";
         return 1;
     }
 
-    int n = stoi(argv[1]);
-    int total_graph_count = stoll(argv[2]);
-
-    int dtg_count = 0;
-    for (int gr_id = 0; gr_id < total_graph_count; gr_id++) {
-        vector<vector<int>> gr(n);
-        for (int v = 0; v < n; v++) {
-            for (int u = v + 1; u < n; u++) {
-                if (rng() & 1) {
-                    gr[v].push_back(u);
-                    gr[u].push_back(v);
-                }
-            }
-        }
-        dtg_count += GraphProcessor::ProcessGraphBasic(gr);
+    int n_l = stoi(argv[1]);
+    int n_r = stoi(argv[2]);
+    int graph_count = stoi(argv[3]);
+    int thread_count = std::thread::hardware_concurrency();
+    if (argc == 5) {
+        thread_count = stoi(argv[4]);
     }
-    cout << dtg_count << '\n';
-    cout << double(dtg_count) / total_graph_count << '\n';
+
+    vector<int> rng_seeds(thread_count);
+    for (int t = 0; t < thread_count; t++) {
+        rng_seeds[t] = rng();
+    }
+
+    for (int n = n_l; n <= n_r; n++) {
+        std::atomic<int> graphs_processed = 0;
+        std::atomic<int> dtg_count = 0;
+
+        auto count_graphs = [&](int t_id) {
+            mt19937 thread_rng(rng_seeds[t_id]);
+            for (int gr_id = 0; gr_id < graph_count; gr_id++) {
+                vector<vector<int>> gr(n);
+                for (int v = 0; v < n; v++) {
+                    for (int u = v + 1; u < n; u++) {
+                        if (rng() & 1) {
+                            gr[v].push_back(u);
+                            gr[u].push_back(v);
+                        }
+                    }
+                }
+                dtg_count += GraphProcessor::ProcessGraphBasic(gr);
+            }
+        };
+
+        vector<std::thread> threads;
+        for (int t = 0; t < thread_count; t++) {
+            threads.emplace_back([&, t]() {
+                mt19937 thread_rng(rng_seeds[t]);
+                while (1) {
+                    if (graphs_processed.fetch_add(1) >= graph_count) {
+                        return;
+                    }
+                    vector<vector<int>> gr(n);
+                    for (int v = 0; v < n; v++) {
+                        for (int u = v + 1; u < n; u++) {
+                            if (thread_rng() & 1) {
+                                gr[v].push_back(u);
+                                gr[u].push_back(v);
+                            }
+                        }
+                    }
+                    dtg_count += GraphProcessor::ProcessGraphBasic(gr);
+                }
+            });
+        }
+        for (int t = 0; t < thread_count; t++) {
+            threads[t].join();
+        }
+        printf("%d: %d\n", n, dtg_count.load());
+    }
 }
